61771b95b1e91bf0b76c8996e057151cc7bac309
chenaoh <branandres6@gmail.com>
2026-06-08 12:03:40 -0500

    configurar conexion

--- config/db.js
+++ config/db.js
@@ -1,9 +1,10 @@
 const { Pool } = require('pg');
+
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'Gimnasio_unam',
-    password: '123456',
-    port: 5432
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
+
 module.exports = pool;
