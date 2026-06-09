fa311c1a484d6a5d93a9a813871a02b418c532c3
chenaoh <branandres6@gmail.com>
2026-06-09 14:52:31 -0500

    base_De_datos

--- config/db.js
+++ config/db.js
@@ -1,9 +1,21 @@
+//const { Pool } = require('pg');
+//const pool = new Pool({
+//    user: 'postgres',
+//    host: 'localhost',
+//    database: 'Gimnasio_unam',
+//    password: '123456',
+//    port: 5432
+//});
+//module.exports = pool;//
+
+
 const { Pool } = require('pg');
+
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'Gimnasio_unam',
-    password: '123456',
-    port: 5432
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
+
 module.exports = pool;
