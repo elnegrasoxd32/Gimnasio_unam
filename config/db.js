d5bfffe721ad41b6f003940866c10941cc7edfa8
chenaoh <branandres6@gmail.com>
2026-06-27 17:48:54 -0500

    bd_act

--- config/db.js
+++ config/db.js
@@ -1,21 +1,21 @@
-const { Pool } = require('pg');
-const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'Gimnasio_unam',
-    password: '123456',
-    port: 5432
-});
-module.exports = pool;
-
-
 //const { Pool } = require('pg');
-//
 //const pool = new Pool({
-//    connectionString: process.env.DATABASE_URL,
-//    ssl: {
-//        rejectUnauthorized: false
-//    }
+//    user: 'postgres',
+//    host: 'localhost',
+//    database: 'Gimnasio_unam',
+//    password: '123456',
+//    port: 5432
 //});
-
 //module.exports = pool;
+
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+module.exports = pool;
